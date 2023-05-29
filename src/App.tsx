8bf9b6452844209cb6741380ef1180976545dd9e
RoshanRv <nkroshankumar@gmail.com>
2023-05-29 16:39:13 +0530

    project setup and tailwind added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import Calendar from "./screens/Calendar"
+import Header from "./components/Header"
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Calendar />
+        </>
+    )
+}
+
+export default App
